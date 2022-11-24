ddf77a0c8379568a08041562c5d3a138e94f034c
wanda <vdking.168@gmail.com>
2022-11-24 22:48:08 +0700

    🚀 go basic unit test

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-basic-unit-test
+
+a simple Go project and implemented unit test based on Table-Driven, Test Coverage, Benchmark and Example approaches.
---
+++ greeting_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func Test_SayHello_ValidArgument(t *testing.T) {
+// 	name := "Mert"
+// 	expected := fmt.Sprintf("Hello %s", name)
+// 	result := sayHello(name)
+
+// 	if result != expected {
+// 		t.Errorf("\"sayHello('%s')\" FAILED, expected -> %v, got -> %v", name, expected, result)
+// 	} else {
+// 		t.Logf("\"sayHello('%s')\" SUCCEDED, expected -> %v, got -> %v", name, expected, result)
+// 	}
+// }
+
+func Test_SayHello_ValidArgument(t *testing.T) {
+	inputs := []struct {
+		name   string
+		result string
+	}{
+		{name: "Yemeksepeti", result: "Hello Yemeksepeti"},
+		{name: "Banabi", result: "Hello Banabi"},
+		{name: "Yemek", result: "Hello Yemek"},
+	}
+
+	for _, item := range inputs {
+
+		result := sayHello(item.name)
+
+		if result != item.result {
+			t.Errorf("\"sayHello('%s')\" failed, expected -> %v, got -> %v", item.name, item.result, result)
+		} else {
+			t.Logf("\"sayHello('%s')\" succeded, expected -> %v, got -> %v", item.name, item.result, result)
+		}
+	}
+}
+
+func Test_SayGoodBye(t *testing.T) {
+	name := "Mert"
+	expected := fmt.Sprintf("Bye Bye %s", name)
+	result := sayGoodBye(name)
+
+	if result != expected {
+		t.Errorf("\"sayGoodBye('%s')\" FAILED, expected -> %v, got -> %v", name, expected, result)
+	} else {
+		t.Logf("\"sayGoodBye('%s')\" SUCCEDED, expected -> %v, got -> %v", name, expected, result)
+	}
+}
+
+func BenchmarkSayHello(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sayHello("Yemeksepeti")
+	}
+}
+
+func ExampleSayHello() {
+	fmt.Println(sayHello("Mert"))
+	// Output: Hello Mert
+}
